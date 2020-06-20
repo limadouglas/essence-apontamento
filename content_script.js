1e2a96c28df6c0e4c81db6825cc39a74bd5dd58a
Douglas Henrique <dhlima.essence@rd.com.br>
2020-06-20 12:17:23 -0300

    new attribute onlyEmpty

--- content_script.js
+++ content_script.js
@@ -2,7 +2,7 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.updateHours){
-      updateHour(request.hours);
+      updateHour(request.hours, request.isOnlyEmpty);
       sendResponse({success: true});
     }
 
@@ -11,26 +11,29 @@ chrome.runtime.onMessage.addListener(
 );
 
 
-function updateHour(hours){
+function updateHour(hours, isOnlyEmpty){
   document.querySelectorAll('.hora').forEach(function(hour){
+
     if(hour.parentNode.style.backgroundColor !== 'rgb(192, 192, 192)'){
       const nameHourSplit = hour.getAttribute('id').split('_');
       const typeHour = nameHourSplit[nameHourSplit.length-1]
       
-      switch(typeHour){
-        case 'txtEntrada1': 
-          hour.value = hours.entryHour1;
-          break;
-        case 'txtSaida1': 
-          hour.value = hours.exitHour1;
-          break;
-        case 'txtEntrada2': 
-          hour.value = hours.entryHour2;
-          break;
-        case 'txtSaida2': 
-          hour.value = hours.exitHour2;
-          break;
-        default: break;
+      if(!isOnlyEmpty || !hour.value.trim()){
+        switch(typeHour){
+          case 'txtEntrada1': 
+            hour.value = hours.entryHour1;
+            break;
+          case 'txtSaida1': 
+            hour.value = hours.exitHour1;
+            break;
+          case 'txtEntrada2': 
+            hour.value = hours.entryHour2;
+            break;
+          case 'txtSaida2': 
+            hour.value = hours.exitHour2;
+            break;
+          default: break;
+        }
       }
     }
   });
--- popup.js
+++ popup.js
@@ -1,35 +1,41 @@
-var entryHour1 = document.getElementById('entryHour1')
-var exitHour1 = document.getElementById('exitHour1')
-var entryHour2 = document.getElementById('entryHour2')
-var exitHour2 = document.getElementById('exitHour2')
+var entryHour1 = document.getElementById('entryHour1');
+var exitHour1 = document.getElementById('exitHour1');
+var entryHour2 = document.getElementById('entryHour2');
+var exitHour2 = document.getElementById('exitHour2');
+
+var hoursSubmit = document.getElementById('hoursSubmit');
+var onlyEmpty =document.getElementById("onlyEmpty");
 
 function displayHours() {
-  chrome.storage.local.get(['hours'], function(object) {
+  chrome.storage.local.get(['hours', 'isOnlyEmpty'], function(object) {
     if(!!object.hours) {
       entryHour1.value = object.hours.entryHour1;
       exitHour1.value = object.hours.exitHour1;
       entryHour2.value = object.hours.entryHour2;
       exitHour2.value = object.hours.exitHour2;
+      onlyEmpty.checked = object.isOnlyEmpty;
     }
   });
 }
 
 displayHours();
 
-document.getElementById('hoursSubmit').onclick = function() {
+hoursSubmit.onclick = function(event) {
   let hours = {
     entryHour1: entryHour1.value, 
     exitHour1: exitHour1.value, 
     entryHour2: entryHour2.value, 
     exitHour2: exitHour2.value
   };
+
+  let isOnlyEmpty = onlyEmpty.checked;
   
-  chrome.storage.local.set({hours}, function(){
+  chrome.storage.local.set({hours, isOnlyEmpty}, function(){
     chrome.tabs.executeScript(null, {
       file: 'content_script.js'
     });
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {updateHours:true, hours}, function(response) {
+      chrome.tabs.sendMessage(tabs[0].id, {updateHours:true, hours, isOnlyEmpty }, function(response) {
         console.log(response);
       });
     });
